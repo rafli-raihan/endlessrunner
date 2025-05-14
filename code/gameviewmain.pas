9d40e1faae18e9d2f622d1d752b145d93816a057
rafli-raihan <mrafliraihan@proton.me>
2025-05-14 16:20:19 +0700

    bjirlah API nya kocak bat sumpahhh, hrs benerin kamera dlu kyanya

--- code/gameviewmain.pas
+++ code/gameviewmain.pas
@@ -7,9 +7,11 @@ unit GameViewMain;
 
 interface
 
-uses Classes,
-  CastleVectors, CastleComponentSerialize,
-  CastleUIControls, CastleControls, CastleKeysMouse;
+uses Classes, castlekeysmouse, math,
+  CastleScene, CastleTransform, CastleVectors, CastleCameras,
+  CastleComponentSerialize, CastleUIControls, CastleControls,
+  CastleViewport, CastleWindow, CastleApplicationProperties,
+  CastleUtils, CastleLog;
 
 type
   { Main view, where most of the application logic takes place. }
@@ -18,7 +20,13 @@ type
     { Components designed using CGE editor.
       These fields will be automatically initialized at Start. }
     LabelFps: TCastleLabel;
+    GameplayViewport: TCastleViewport;
+    GroundGroup: TCastleTransform;
+    Camera1: TCastleCamera;
+    procedure UpdateGround;
   public
+    GroundTiles: array of TCastleScene;
+    TileWidth: Single;
     constructor Create(AOwner: TComponent); override;
     procedure Start; override;
     procedure Update(const SecondsPassed: Single; var HandleInput: Boolean); override;
@@ -41,8 +49,45 @@ begin
 end;
 
 procedure TViewMain.Start;
+var
+  I, TileCount: Integer;
+  ViewWidth: Single;
+  Tile: TCastleScene;
 begin
   inherited;
+
+  TileWidth := 128.0;
+
+  ViewWidth := Camera1.Orthographic.Width;
+  TileCount := Ceil(ViewWidth / TileWidth) + 2;
+
+  SetLength(GroundTiles, TileCount);
+  for I := 0 to TileCount - 1 do
+  begin
+    Tile := TCastleScene.Create(FreeAtStop);
+    Tile.Load('castle-data:/assets/ground/ground.png');
+    Tile.Translation := Vector3(I * TileWidth, 0, 3);
+    GroundGroup.Add(Tile);
+    GroundTiles[I] := Tile;
+  end;
+end;
+
+procedure TViewMain.UpdateGround;
+var
+  I: Integer;
+  CamX, StartX: Single;
+begin
+  CamX := Camera1.Translation.X;
+  StartX := Floor(CamX / TileWidth) * TileWidth;
+
+  for I := 0 to High(GroundTiles) do
+  begin
+    GroundTiles[I].Translation := Vector3(
+      StartX + I * TileWidth,
+      GroundTiles[I].Translation.Y,
+      3
+    );
+  end;
 end;
 
 procedure TViewMain.Update(const SecondsPassed: Single; var HandleInput: Boolean);
@@ -51,6 +96,7 @@ begin
   { This virtual method is executed every frame (many times per second). }
   Assert(LabelFps <> nil, 'If you remove LabelFps from the design, remember to remove also the assignment "LabelFps.Caption := ..." from code');
   LabelFps.Caption := 'FPS: ' + Container.Fps.ToString;
+  UpdateGround;
 end;
 
 function TViewMain.Press(const Event: TInputPressRelease): Boolean;
